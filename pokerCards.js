f20b42653ac1a3953df63e360775ded24585ea18
Lorena Criado <criado.manzaneque@gmail.com>
2024-10-09 20:17:00 +0200

    Pokeer Cards solution has been added

--- pokerCards.js
+++ pokerCards.js
@@ -1,9 +1,15 @@
 function shuffleDeck(deck) {
     return deck.sort(() => Math.random() - 0.5); // Función para mezclar el array
+    // El método .sort() normalmente organiza los elementos de un array en base a una comparación.
+    // Aquí, usamos una función de comparación que devuelve un valor aleatorio entre -0.5 y 0.5 (Math.random() genera un número entre 0 y 1, y restar 0.5 lo transforma en un valor entre -0.5 y 0.5).
+    // Como el método .sort() organiza los elementos en base a si la función de comparación devuelve un valor positivo o negativo, esta operación aleatoria desordena la baraja.
 }
 
 function selectCard(deck, card) {
+    // Busca el índice de la carta card dentro de la baraja deck.
+    // Si la carta está en la baraja, cardIndex será un número mayor o igual a 0; si no está, será -1.
     const cardIndex = deck.indexOf(card);
+    // Si cardIndex es mayor que -1, significa que la carta fue encontrada en la baraja.
     if (cardIndex > -1) {
         const removedCard = deck.splice(cardIndex, 1)[0]; // Elimina la carta
         shuffleDeck(deck); // Mezcla la baraja después de eliminar la carta
@@ -12,3 +18,4 @@ function selectCard(deck, card) {
     return undefined; // La carta no estaba en la baraja
 }
 
+module.exports = { selectCard };
--- pokerCards.test.js
+++ pokerCards.test.js
@@ -1,1 +1,31 @@
-// TODO: Importar selectCard !!
+const { selectCard } = require("./pokerCards");
+
+it('should select a card if it exists in deck and remove it', () => {
+    const deck = ['A♠', '2♠', '3♠', '4♠', '5♠'];
+    const selectedCard = selectCard(deck, '3♠');
+    expect(selectedCard).toBe('3♠');
+    expect(deck).not.toContain('3♠');
+    expect(deck).toHaveLength(4);
+});
+
+it('should select the first card of deck and remove it', () => {
+    const deck = ['A♥', 'K♦', '10♣', 'J♠'];
+    const selectedCard = selectCard(deck, 'A♥');
+    expect(selectedCard).toBe('A♥');
+    expect(deck).not.toContain('A♥');
+    expect(deck).toHaveLength(3);
+})
+
+it('should return "undefined" when the card is not in deck, and the deck length has to be the same', () => {
+    const deck = ['7♠', '8♦', '9♣'];
+    const selectedCard = selectCard(deck, '10♣');
+    expect(selectedCard).toBe(undefined);
+    expect(deck).toHaveLength(3);
+});
+
+
+// it('should select a card if it exists in deck and remove it', () => {
+//     const deck = ['A♥', 'K♦', '10♣', 'J♠'];
+//     const selectedCard = selectCard(deck, 'A♥');
+//     expect(selectedCard).toBe('A♥')
+// });
