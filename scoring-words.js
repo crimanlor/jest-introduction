b8b95c6ab79395ab84a2b55e221600f79cec362c
Lorena Criado <criado.manzaneque@gmail.com>
2024-10-09 12:56:59 +0200

    First iterations have been resolved

--- scoring-words.js
+++ scoring-words.js
@@ -8,11 +8,9 @@
 
 function high(x) {
     // PRIMERO: Darle un valor númerico a cada letra. 
-    //Se me ocurre hacer un array con las letras donde su valor sea su posición en el index +1
+    //Array con todas las letras donde su valor sea su posición en el index +1 (se lo daremos después con indexOf() +1 )
 
-    const letters = ['a', , 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-    // Al intentar poner esto dentro de una variable descubro metodo bind(). VER DESPUES
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     //SEGUNDO separar las palabras de una frase y volverlas elementos de un array
     let arrayPhrase = x.split(" ");
@@ -23,21 +21,25 @@ function high(x) {
     //Otra para poner cual es la palabra con más puntos
     let maxWord = "";
 
-    // Recorrer cada palabra (array) para poner comparar cuál tiene más puntos
+    // Recorrer cada palabra (array) para poder comparar cuál tiene más puntos
     for (let w of arrayPhrase) {
-
-        //contar los puntos de cada palabra iterando dentro de la misma  
-        let pointstWord = 0;
-        for (let i of w) {
-            pointstWord += letters.indexOf(i) + 1;
+        // Si la palabra contiene todas las volales, se devuelve automáticamente sin contar los puntos
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
+        if (vowels.every(vowel => w.includes(vowel))) {
+            return w;
         }
 
+        //Contar los puntos de cada palabra iterando dentro sus letras (si no contiene todas las vocales)
+        let pointstWord = 0;
+        for (let i of w) {
+            // Para cada letra i, se busca su índice en el array letters con letters.indexOf(i) y se le suma 1 (ya que el índice comienza en 0 pero queremos que el valor de 'a' sea 1).
+            pointstWord += letters.indexOf(i) + 1;
+        }
+        // Después de contar los puntos de la palabra w, se compara con maxPoint. Si la palabra actual tiene más puntos que la palabra con más puntos hasta ese momento, se actualizan maxPoint y maxWord con los nuevos valores.
         if (maxPoint < pointstWord) {
             maxPoint = pointstWord;
             maxWord = w;
         }
-
-
     }
 
     return maxWord;
--- scoring-words.test.js
+++ scoring-words.test.js
@@ -1,1 +1,39 @@
 const { high } = require('./scoring-words'); 
+
+it('Should return taxi for "man i need a taxi up to ubud"', () => {
+    const words = "man i need a taxi up to ubud";
+    const maxPointsWord = high(words);
+    expect(maxPointsWord).toBe("taxi");
+})
+
+it('Should return volcano for "what time are we climbing up the volcano"', () => {
+    const words = "what time are we climbing up the volcano";
+    const maxPointsWord = high(words);
+    expect(maxPointsWord).toBe("volcano")
+})
+
+it('Should return semynak for "take me to semynak"', () => {
+    const words = "take me to semynak";
+    const maxPointsWord = high(words);
+    expect(maxPointsWord).toBe("semynak")
+})
+
+it('Should return aa for "aa b"', () => {
+    const words = "aa b";
+    const maxPointsWord = high(words);
+    expect(maxPointsWord).toBe("aa")
+})
+
+it('should return the only word in the case the string has only one word', () => {
+    const words = "pepe";
+    const maxPointsWord = high(words);
+    expect(maxPointsWord).toBe("pepe");
+})
+
+it('should return a word with all the vowels in case there is one present', () => {
+    const words = "I think education is the most valuable investment in a modern state";
+
+    const maxPointsWord = high(words);
+
+    expect(maxPointsWord).toBe("education");
+})
